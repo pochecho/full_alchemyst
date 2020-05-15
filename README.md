996a9f69efeb8136f14374c3aa1e878055112099
Sergio Posada Urrea <sergio.posadaurrea@gmail.com>
2020-05-14 22:51:31 -0500

    Deploy pip

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # full_alchemyst
 Let's you to use both SqlAlchemyst and MongoAlchemyst libraries
+Les's to use a unique ORM to manage the persistence in one project. 
--- setup.py
+++ setup.py
@@ -1,13 +1,24 @@
-from distutils.core import setup
-setup(
-  name = 'full_alchemyst',
-  packages = ['full_alchemyst'], # this must be the same as the name above
-  version = '0.0.1',
-  description = "Les's to use a unique ORM to manage the persistence in one project. ",
-  author = 'Sergio Posada Urrea',
-  author_email = 'alejandro@geekytheory.com',
-  url = 'https://github.com/{user_name}/{repo}', # use the URL to the github repo
-  download_url = 'https://github.com/{user_name}/{repo}/tarball/0.1',
-  keywords = ['testing', 'logging', 'example'],
-  classifiers = [],
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+setuptools.setup(
+    name = 'full_alchemyst-pochecho',
+    version = '0.0.1',
+    description = "Les's to use a unique ORM to manage the persistence in one project. ",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    author = 'Sergio Posada Urrea',
+    author_email = 'sergio.posadaurrea@gmail.com',
+    url = 'https://github.com/pochecho/full_alchemyst',
+    packages = setuptools.find_packages(),
+    download_url = 'https://github.com/pochecho/full_alchemyst.git',
+    keywords = ['MongoAlchemyst', 'SqlAlchemyst', 'Flask','DB'],
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+        ],
+    python_requires = ">=3.6"
 )
